609e82c61814904330ac425b71df4b3515d7f5fb
Miguel Arturo Sanchez Cuello <miguelsc10@hotmail.com>
2022-09-28 17:00:10 -0400

    added the contact form with php to test it

--- forms/contact copy.php
+++ forms/contact copy.php
@@ -1,20 +1,4 @@
 <?php
-  if(isset($_POST['submit']){
-    $name = $_POST['name'];
-    $mailFrom = $_POST['email'];
-    $message = $_POST['message'];
-    $subject = $_POST['subject'];
-
-    $mailTo = "miguelsc10@hotmail.com";
-    $headers = "From ".$mailFrom;
-    $txt = "You have received an e-mail from ".$name."\n\n".$message;
-
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.html?mailsend");
-  }
-  
-  
-
   /**
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
@@ -23,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  /* $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'contact@example.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -37,7 +21,7 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject']; */
+  $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -49,9 +33,9 @@
   );
   */
 
-  /* $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send(); */
+  echo $contact->send();
 ?>
